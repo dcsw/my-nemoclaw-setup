ed1b34effd7875a4c67dfac445f3c7ac8375a786
dcsw <dcirielli5@gmail.com>
2026-09-20 14:46:19 -0400

    Reset main to branch Article-2

--- change-sandbox-model.sh
+++ change-sandbox-model.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-# export NEMOCLAW_MODEL=${1:-nvidia/nemotron-3-super-120b-a12b}
+# export NEMOCLAW_MODEL=${1:-nvidia/nemotron-3-super-120b-a12b}?
 export NEMOCLAW_MODEL=${1:-nvidia/nemotron-3-ultra-550b-a55b}
 echo Changing model to ${NEMOCLAW_MODEL} 
 
